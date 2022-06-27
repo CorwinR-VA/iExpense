2bf3660c422bf9984105294dae299f39a5a4fc4f
Corwin Rainier <corwinrainier89@gmail.com>
2022-06-27 13:27:30 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Corwin Rainier on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
